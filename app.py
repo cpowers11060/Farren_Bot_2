3e287cf3b3eddb84cc675a6ab4f98cd380bcb73e
Chris Powers <c-11060@my.uri.edu>
2020-05-14 18:06:15 -0400

    testing my own message

--- app.py
+++ app.py
@@ -7,11 +7,30 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+'''
+Note that the group me message api is based on the following
+dictionary:
+{
+  "attachments": [],
+  "avatar_url": "http://i.groupme.com/123456789",
+  "created_at": 1302623328,
+  "group_id": "1234567890",
+  "id": "1234567890",
+  "name": "John",
+  "sender_id": "12345",
+  "sender_type": "user",
+  "source_guid": "GUID",
+  "system": false,
+  "text": "Hello world ☃☃",
+  "user_id": "1234567890"
+}
+'''
 
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
-
+  if 'hot' in data['text']:
+    msg = 'Stay hot and beautiful, {}. I love you.'.format(data['name']
   # We don't want to reply to ourselves!
   #if data['name'] != 'Chris Farren Bot':
     #msg = '{}, you sent "{}".'.format(data['name'], data['text'])
